47fb90fb253a4f8646f89236525377a870f2fc29
Citraloka <103099254+Citraloka@users.noreply.github.com>
2022-12-20 16:27:40 +0700

    Update multimu.js

--- multimu.js
+++ multimu.js
@@ -2,11 +2,20 @@ const readlineSync  = require("readline-sync");
 const fetch = require('node-fetch');
 const fs = require('fs');
 const cw = require('crypto-wallets');
+const randomUseragent = require('random-useragent');
 
+const genhead = randomUseragent.getRandom(function (ua) {
+    return ua.browserName === 'Chrome';
+});
 
     //----------------------------------------------------------------------------------------------------------------------------  
+    const ua = {
+        'content-type' : 'text/plain',
+        'user-agent' : genhead,
+        'origin' : 'https://collectibles.manutd.com',
+        'referer' : 'https://collectibles.manutd.com/',
+    };
 
-    
     const getapi = async (url) => {
         var req_f      = fetch(url);
         //result JSON
@@ -28,7 +37,8 @@ const cw = require('crypto-wallets');
     }
 
     const incaptcha = async (key, sitegooglekey) => {
-        var req_f      = fetch('http://2captcha.com/in.php?key='+key+'&method=userrecaptcha&json=1&googlekey='+sitegooglekey+'&invisible=1&pageurl=https://collectibles.manutd.com/api/claim-token&userAgent=Mozilla/5.0%20(Windows%20NT%2010.0;%20Win64;%20x64)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/108.0.0.0%20Safari/537.36');
+        var head = encodeURI(ua["user-agent"]);
+        var req_f      = fetch('http://2captcha.com/in.php?key='+key+'&method=userrecaptcha&json=1&googlekey='+sitegooglekey+'&invisible=1&pageurl=https://collectibles.manutd.com/api/claim-token&userAgent='+head);
         //result JSON
         var res_f      = req_f.then(res => res.json());
 
@@ -77,12 +87,7 @@ const cw = require('crypto-wallets');
     const pertama = async (wallet_tz) => {
         var data_wallet     = `{"wallet_address": "${wallet_tz}","wallet_provider": "Temple - Tezos Wallet"}`;
         var url_wallet      = "https://collectibles.manutd.com/api/claim-wallet";
-        var head_wallet     = {
-            'content-type' : 'text/plain',
-            'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
-            'origin' : 'https://collectibles.manutd.com',
-            'referer' : 'https://collectibles.manutd.com/',
-        };
+        var head_wallet     = ua;
         var req_wallet      = await postapi(url_wallet, data_wallet, head_wallet);
 
         console.log('    [-] Register Wallet | ' + wallet_tz);
@@ -92,24 +97,23 @@ const cw = require('crypto-wallets');
     }
 
     const kedua = async (wallet_tz) => {
-        const email   = await randgmail();
-        const gmail   = email["username"]+'@gmail.com';
+        var email   = await randgmail();
+        var gmail   = email["username"]+'@gmail.com';
         // console.log('[-]  Gmail | ' + gmail);
 
         var data_email  = `{"wallet_address": "${wallet_tz}","email": "${gmail}","marketing_consent": true}`;
         var url_email   = "https://collectibles.manutd.com/api/claim-email";
-        var head_email  = {
-            'content-type' : 'text/plain',
-            'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
-            'origin' : 'https://collectibles.manutd.com',
-            'referer' : 'https://collectibles.manutd.com/',
-            'cookie' : 's_cc=true; wallet_address="'+wallet_tz+'"; wallet_type=beacon; phase_two_onboarding_step=1',
-        };
+        var head_email  = ua;
+        head_email.cookie = 's_cc=true; wallet_address="'+wallet_tz+'"; wallet_type=beacon; phase_two_onboarding_step=1';
+        do{
         var req_email      = await postapi(url_email, data_email, head_email);
+        } while (req_email == null)
 
         
         console.log('    [-] Register Email | ' + gmail);
         // console.log(req_email);
+
+        return gmail;
     }
 
     const ketiga = async (wallet_tz, pkey, key2captcha, googlekey) => {
@@ -123,13 +127,9 @@ const cw = require('crypto-wallets');
                 var recaptchaToken = authbypas.request;
             } while (recaptchaToken === "CAPCHA_NOT_READY");
 
-        var head_claim  = {
-            'content-type' : 'text/plain;charset=UTF-8',
-            'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
-            'origin' : 'https://collectibles.manutd.com',
-            'referer' : 'https://collectibles.manutd.com/',
-            'cookie' : 's_cc=true; wallet_address="'+wallet_tz+'"; wallet_type=beacon; phase_two_onboarding_step=4',
-        };
+        var head_claim  = ua;
+        head_claim.cookie = 's_cc=true; wallet_address="'+wallet_tz+'"; wallet_type=beacon; phase_two_onboarding_step=4';
+
         var data_claim  = `{"wallet_address": "${wallet_tz}","recaptcha_response": "${recaptchaToken}"}`;
         var url_claim   = "https://collectibles.manutd.com/api/claim-token";
         var req_claim   = await postapi(url_claim, data_claim, head_claim);
@@ -150,7 +150,7 @@ const cw = require('crypto-wallets');
             console.log("        - Id   > "+id_nft);
 
             //save data wallet
-            var text_save = `${type_nft}|${name_nft}|${wallet_tz}|${pkey}\n`;
+            var text_save = `${type_nft}|${name_nft}|${wallet_tz}|${pkey}`;
             await save(text_save, 'result_wallet.txt');
             console.log("    [✓] Save data to file");
         } else {
@@ -162,6 +162,7 @@ const cw = require('crypto-wallets');
     }
 
     const a = async (index) => {
+
         var data_x = await sipaling();
         var key2captcha = data_x.key2captcha;
         var googlekey = data_x.googlekey;
@@ -202,10 +203,14 @@ const cw = require('crypto-wallets');
             }
         } else {
             await delay(3000);
-            await kedua(wallet_tz);
+            var gmail2 = await kedua(wallet_tz);
 
             await delay(3000);
             await ketiga(wallet_tz, privkey, key2captcha, googlekey);
+
+            //save gmail data wallet
+            var text_save = `|${gmail2}\n`;
+            await save(text_save, 'result_wallet.txt');
         }
     }
 
